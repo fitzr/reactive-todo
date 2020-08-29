a1f9300a72e788c94dfd002a3f9f67f9eda0147f
Takehiro Hagiwara <hagiwara.takehiro@classmethod.jp>
2020-08-29 09:27:00 +0900

    first commit

---
+++ src/main/kotlin/com/example/reactivetodo/ReactiveTodoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.reactivetodo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactiveTodoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReactiveTodoApplication>(*args)
+}
