fa1f836e6b8098254c2dfe31075fab0f585afa24
Takehiro Hagiwara <hagiwara.takehiro@classmethod.jp>
2020-08-30 23:18:25 +0900

    fix schema.sql

--- local/postgres/ddl/schema.sql
+++ local/postgres/ddl/schema.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE reactivetodo ENCODING 'UTF8';
-USE reactivetodo;
 CREATE TABLE IF NOT EXISTS todos(
     id serial PRIMARY KEY,
     user_id varchar(255) NOT NULL,
@@ -7,4 +5,3 @@ CREATE TABLE IF NOT EXISTS todos(
     content text NOT NULL,
     done boolean NOT NULL DEFAULT false
 );
-TRUNCATE TABLE todos;
